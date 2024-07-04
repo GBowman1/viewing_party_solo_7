b55af01ce573783b0cadd3f9e98567bbe6107bfb
Garrett Bowman <garrettbowman248@gmail.com>
2024-07-04 15:46:38 -0500

    wip"

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,24 +1,28 @@
 class MoviesController < ApplicationController
     def index
         @user = User.find(params[:user_id])
-        if params[:movie].present?
-            conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
-                faraday.headers['X-API-Key'] = Rails.application.credentials.tmdb[:key]
-            end
-            response = conn.get("/3/search/movie?query=#{params[:movie]}")
-
-            json = JSON.parse(response.body, symbolize_names: true)
-            @movies = json[:results]
-        elsif params[:top_rated].present?
+        if params[:search] == 'top_rated'
             conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
                 faraday.headers['X-API-Key'] = Rails.application.credentials.tmdb[:key]
             end
             response = conn.get('3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc')
+            # require 'pry';binding.pry
+
+            json = JSON.parse(response.body, symbolize_names: true)
+            @movies = json[:results]
+        elsif params[:search] == 'query'
+            conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+                faraday.headers['X-API-Key'] = Rails.application.credentials.tmdb[:key]
+            end
+            search = params[:movie].gsub(' ', '%20')
+            response = conn.get("/3/search/movie?query=#{search}")
 
             json = JSON.parse(response.body, symbolize_names: true)
             @movies = json[:results]
         else 
             @movies = []
         end
+        # require 'pry';binding.pry
     end
+
 end
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Movies Index Page' do
     feature 'visitor can view movies results' do
         before :each do
             @user = User.create!(name: 'Mike Tyson', email: 'miketyson@aol.com')
-            @json_response = JSON.parse(File.read('spec/fixtures/top_rated_fixture.json'))
+            @json_response = File.read('spec/fixtures/top_rated_fixture.json')
             stub_request(:get, "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc").with(
                 headers: {
                     'Accept'=>'*/*',
@@ -13,7 +13,7 @@ RSpec.describe 'Movies Index Page' do
                     'X-Api-Key'=>'aa8f476e09c282147d59ae9a584801f6'
                     }).to_return(status: 200, body: @json_response.to_json, headers: {})
                     
-            @search = JSON.parse(File.read('spec/fixtures/search_fixture.json'))
+            @search = File.read('spec/fixtures/search_fixture.json')
             stub_request(:get, "https://api.themoviedb.org/3/search/movie?query=The%20Godfather").with(
                 headers: {
                 'Accept'=>'*/*',
@@ -22,6 +22,7 @@ RSpec.describe 'Movies Index Page' do
                 'X-Api-Key'=>'aa8f476e09c282147d59ae9a584801f6'
                 }).
                 to_return(status: 200, body: @search.to_json, headers: {})
+                # require 'pry';binding.pry
         end
 
         it 'visitor can see top rated movies' do
@@ -31,7 +32,8 @@ RSpec.describe 'Movies Index Page' do
             end
             expect(current_path).to eq(user_movies_path(@user))
 
-            expect(@json_response['results'].count).to eq(20)
+            # expect(@json_response['results'].count).to eq(20)
+            
 
             within '#movie_1' do
                 expect(page).to have_link("Furiosa: A Mad Max Saga")
